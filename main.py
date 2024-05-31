af8eb76ccb538b515f7a1d73069d2f45ea072c9d
cohsh <82075327+cohsh@users.noreply.github.com>
2024-05-31 16:29:19 +0900

    :zap: changed font setting style

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ texts = {'title': 'Title', \
         'discussion': 'Discussion', \
         'next': 'Read next'}
 
-font_big = {'name': 'Source Code Pro', 'size': 24}
-font_small = {'name': 'Source Code Pro', 'size': 12}
+font = 'Source Code Pro'
+
+small_font = (font, 12)
+big_font = (font, 24)
+
 size = [400, 600]
 space = int(size[0]*0.02)
 
@@ -32,38 +35,38 @@ def main():
     root.geometry(str(size[0])+'x'+str(size[1]))
     root.title('Survey Tool')
 
-    subtitle = tkinter.Label(text='Survey Tool', font=(font_big['name'], font_big['size']))
+    subtitle = tkinter.Label(text='Survey Tool', font=big_font)
     subtitle.place(x=0, y=y0)
 
     add_spacing(2)
 
-    doi_label = tkinter.Label(text='DOI', font=(font_small['name'], font_small['size']))
+    doi_label = tkinter.Label(text='DOI', font=small_font)
     doi_label.place(x=0, y=y0)
 
     add_spacing(1)
 
-    doi_input = tkinter.Text(width=50, height=1, font=(font_small['name'], font_small['size']))
+    doi_input = tkinter.Text(width=50, height=1, font=small_font)
     doi_input.place(x=0, y=y0)
 
     add_spacing(1)
 
-    doi_btn = tkinter.Button(root, text='Get', command=lambda: get_paper(doi_input), font=(font_small['name'], font_small['size']))
+    doi_btn = tkinter.Button(root, text='Get', command=lambda: get_paper(doi_input), font=small_font)
     doi_btn.place(x=0, y=y0)
     
     add_spacing(1)
 
     for key in list(texts.keys()):
-        labels[key] = tkinter.Label(text=texts[key], font=(font_small['name'], font_small['size']))
+        labels[key] = tkinter.Label(text=texts[key], font=small_font)
         labels[key].place(x=0, y=y0)
 
         add_spacing(1)
 
-        inputs[key] = tkinter.Text(width=50, height=3, font=(font_small['name'], font_small['size']))
+        inputs[key] = tkinter.Text(width=50, height=3, font=small_font)
         inputs[key].place(x=0, y=y0)
 
         add_spacing(3)
 
-    btn_save = tkinter.Button(root, text='Save', command=lambda: save_text(words), font=(font_small['name'], font_small['size']))
+    btn_save = tkinter.Button(root, text='Save', command=lambda: save_text(words), font=small_font)
     btn_save.place(x=0, y=y0)
 
     root.state('normal')
